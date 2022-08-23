c13d6b3ccb844776bad62625d6a9baa32eb432dd
unknown <ekimumit@hotmail.com>
2022-08-24 00:47:02 +0300

    Add all my files

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+
+
+const Colors = {
+    primary500: '#72063c',
+    primary600: '#640233',
+    accent500: '#ddb52f',
+    primary800: '#3b021f',
+    primary700:'#4e0329',
+
+};
+
+export default Colors; 
